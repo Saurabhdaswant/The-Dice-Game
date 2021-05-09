ad3acf5c24cf32efde904b1c6e9293671d3c340c
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-09 20:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold  ");
 
 //starting conditions
-diceEl.classList.add("hidden");
+// diceEl.classList.add("hidden");
 score1El.textContent = 0;
 score0El.textContent = 0;
 let currentScore = 0;
